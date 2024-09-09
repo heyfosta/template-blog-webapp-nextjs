f2550442092f1308b10a54627aed371fe6f01cd8
heyfosta <48523069+heyfosta@users.noreply.github.com>
2024-09-09 18:16:37 +0100

    Update test-env.page.ts

--- src/pages/api/test-env.page.ts
+++ src/pages/api/test-env.page.ts
@@ -4,11 +4,11 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log('STRIPE_WEBHOOK_SECRET:', process.env.STRIPE_WEBHOOK_SECRET ? 'Set' : 'Not set');
-  console.log('STRIPE_SECRET_KEY:', process.env.STRIPE_SECRET_KEY ? 'Set' : 'Not set');
+  console.log('STRIPE_WEBHOOK_SECRET:', process.env.STRIPE_WEBHOOK_SECRET);
+  console.log('STRIPE_SECRET_KEY:', process.env.STRIPE_SECRET_KEY);
   
   res.status(200).json({
-    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET ? 'Set' : 'Not set',
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY ? 'Set' : 'Not set'
+    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY
   })
 }
